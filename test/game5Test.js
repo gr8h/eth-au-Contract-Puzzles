e4109ddc304552789ca1bb649d07ff5ee3a68313
gr8h <hesham.shabana@hotmail.com>
2022-12-23 08:45:58 +0100

    game5 test update

--- test/game5Test.js
+++ test/game5Test.js
@@ -1,19 +1,23 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
 
-describe('Game5', function () {
+describe("Game5", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game5');
+    const Game = await ethers.getContractFactory("Game5");
     const game = await Game.deploy();
 
     return { game };
   }
-  it('should be a winner', async function () {
+  it("should be a winner", async function () {
     const { game } = await loadFixture(deployContractAndSetVariables);
 
     // Get address < target address
     let wallet = ethers.Wallet.createRandom();
-    while(BigInt(wallet.address) >= BigInt("0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf")) {
+    // while(BigInt(wallet.address) >= BigInt("0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf")) {
+    
+    // The contract threshold starts with 0x00, 0x is hexadecimal prefix,
+    //  so if we have an address that has any values other than 00 it will be > threshold
+    while (wallet.address.slice(0, 4) !== "0x00") {
       wallet = ethers.Wallet.createRandom();
     }
 
@@ -21,12 +25,12 @@ describe('Game5', function () {
     const signer = ethers.provider.getSigner(0);
     await signer.sendTransaction({
       to: wallet.address,
-      value: ethers.utils.parseEther("1")
+      value: ethers.utils.parseEther("1"),
     });
 
     await game.connect(wallet.connect(ethers.provider)).win();
 
     // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
